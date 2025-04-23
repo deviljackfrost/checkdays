6dbb9ba89ec9cc9116dd8fd349d57feca610a76c
YoshidaRyoma <178420890+deviljackfrost@users.noreply.github.com>
2025-04-23 13:52:36 +0000

    fase2

--- db/migrate/20250219142633_devise_create_users.rb
+++ db/migrate/20250219142633_devise_create_users.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-class Public::DeviseCreateUsers < ActiveRecord::Migration[6.1]
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
